0a8dcdc944c9f1b790b5508dde9f03f0e2f325e0
githubriyaz1 <seyadriyaz0@gmail.com>
2026-05-09 12:23:31 +0530

    Fixed API URL

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const API = "https://crudoperations-eh5h.onrender.com/";
+  const API = "https://crudoperations-eh5h.onrender.com";
 
   const [users, setUsers] = useState([]);
 
